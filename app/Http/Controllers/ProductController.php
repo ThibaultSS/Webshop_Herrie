769887ab7d4c1b4cff3419b78e64fbf6028fc44b
ThibaultSS <thibault.soumoy@student.kdg.be>
2025-08-24 16:38:23 +0200

    nieuwe json request aangemaakt

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,4 +17,15 @@ class ProductController extends Controller
         }
         return response()->json($products);
     }
+    function getProductById($id)
+    {
+        $product = Product::find($id);
+        if ($product) {
+            $images = Image::where('product_id', $product->id)->get();
+            $product->images = $images;
+            return response()->json($product);
+        } else {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('astro/get/products', [\App\Http\Controllers\ProductController::class, 'getProducts']);
+Route::get('astro/get/products/{id}', [\App\Http\Controllers\ProductController::class, 'getProductById']);
